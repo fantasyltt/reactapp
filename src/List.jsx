076af473cb0a7e3f42f38d00e766a715ba1b2e98
fantasyltt <8631142@qq.com>
2022-06-14 17:25:43 +0800

    排序修改

--- src/List.jsx
+++ src/List.jsx
@@ -38,330 +38,392 @@ const List = (props) =>{
   const caseList = [
     {
     id:0,
-    title:'金桔汇 APP 最新改版',
+    rank:1,
+    title:'SVO APP 新项目',
     type:'APP',
     year:2021
     },
     {
       id:1,
+      rank:0,
       title:'金桔汇 APP 最新改版',
       type:'APP',
       year:2021
     },
     {
       id:2,
+      rank:2,
       title:'分享行动落地页',
       type:'网页/H5',
       year:2021
     },
     {
       id:3,
+      rank:3,
       title:'SovFx 日文圣诞活动',
       type:'网页/H5',
       year:2021
     },
     {
       id:4,
+      rank:4,
       title:'Interpret CMS',
       type:'网页',
       year:2021
     },
     {
       id:5,
+      rank:5,
       title:'Lottie 动画 icon',
       type:'视频动画',
       year:2021
     },
     {
       id:6,
+      rank:6,
       title:'感恩老用户活动',
       type:'网页',
       year:2021
     },
     {
       id:7,
+      rank:7,
       title:'GM 代理联盟计划',
       type:'网页',
       year:2021
     },
     {
       id:8,
+      rank:8,
       title:'SovFx 日文新年活动',
       type:'网页/H5',
       year:2021
     },
     {
       id:9,
+      rank:9,
       title:'ForFX 官网',
       type:'网页',
       year:2021
     },
     {
       id:10,
+      rank:10,
       title:'LinkWits 官网',
       type:'网页',
       year:2021
     },
     {
       id:11,
+      rank:11,
       title:'SmartFX 官网',
       type:'网页',
       year:2021
     },
     {
       id:12,
+      rank:12,
       title:'GM 新年200%赠金活动',
       type:'网页/H5',
       year:2021
     },
     {
       id:13,
+      rank:13,
       title:'GM APP 抽奖活动',
       type:'H5',
       year:2021
     },
     {
+      id:14,
+      rank:14,
+      title:'88交易联赛',
+      type:'网页/H5',
+      year:2021
+    },
+    {
       id:15,
+      rank:15,
       title:'行情速递落地页',
       type:'网页/H5',
       year:2021
     },
     {
       id:16,
+      rank:16,
       title:'0佣金活动',
       type:'H5',
       year:2021
     },
     {
       id:17,
+      rank:17,
       title:'盈汇黄金推广落地页',
       type:'H5',
       year:2021
     },
     {
       id:18,
+      rank:18,
       title:'FXNavi 训练营',
       type:'网页',
       year:2021
     },
     {
       id:19,
+      rank:19,
       title:'FXNavi 落地页',
       type:'网页',
       year:2021
     },
     {
       id:20,
+      rank:20,
       title:'FXNavi 日文落地页',
       type:'网页',
       year:2021
     },
     {
       id:21,
+      rank:21,
       title:'各类广告banner设计',
       type:'广告',
       year:2021
     },
     {
       id:22,
+      rank:22,
       title:'金桔汇 App Store 上架图',
       type:'其他',
       year:2021
     },
     {
       id:23,
+      rank:23,
       title:'金桔汇 App 引导页',
       type:'网页/H5',
       year:2021
     },
     {
       id:24,
+      rank:24,
       title:'APP启动页',
       type:'广告',
       year:2021
     },
     {
       id:25,
+      rank:25,
       title:'优势介绍落地页',
       type:'H5',
       year:2021
     },
     {
       id:26,
+      rank:26,
       title:'新客活动',
       type:'网页/H5',
       year:2021
     },
     {
       id:27,
+      rank:27,
       title:'介绍外汇落地页',
       type:'H5',
       year:2021
     },
     {
       id:28,
+      rank:28,
       title:'200%赠金活动',
       type:'网页/H5',
       year:2021
     },
     {
       id:29,
+      rank:29,
       title:'LinkWits App 引导页',
       type:'其他',
       year:2021
     },
     {
       id:30,
+      rank:30,
       title:'App 引导页',
       type:'其他',
       year:2021
     },
     {
       id:31,
+      rank:31,
       title:'新年氛围 icon',
       type:'其他',
       year:2021
     },
     {
       id:32,
+      rank:32,
       title:'圣诞氛围 icon',
       type:'其他',
       year:2021
     },
     {
       id:33,
+      rank:33,
       title:'广告（2019前）',
       type:'广告',
       year:2019
     },
     {
       id:34,
+      rank:34,
       title:'QTC App（2019前）',
       type:'APP',
       year:2019
     },
     {
       id:35,
+      rank:35,
       title:'速易网络合作方后台管理系统（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:36,
+      rank:36,
       title:'车保宝官方网站（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:37,
+      rank:37,
       title:'帮帮优贷（2019前）',
       type:'APP',
       year:2019
     },
     {
       id:38,
+      rank:38,
       title:'H5推广页面（2019前）',
       type:'H5',
       year:2019
     },
     {
       id:39,
+      rank:39,
       title:'H5推广页面（2019前）',
       type:'H5',
       year:2019
     },
     {
       id:40,
+      rank:40,
       title:'广告集合（2019前）',
       type:'广告',
       year:2019
     },
     {
       id:41,
+      rank:41,
       title:'信息流广告集合（2019前）',
       type:'广告',
       year:2019
     },
     {
       id:42,
+      rank:42,
       title:'生物源官方网站（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:43,
+      rank:43,
       title:'OVONIC官网（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:44,
+      rank:44,
       title:'MAGPOC官网（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:45,
+      rank:45,
       title:'洲安集团官网（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:46,
+      rank:46,
       title:'校园微电影大赛落地页（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:47,
+      rank:47,
       title:'正祺集团官网（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:48,
+      rank:48,
       title:'中原地产海外房产（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:49,
+      rank:49,
       title:'CBP国际教育（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:50,
+      rank:50,
       title:'深圳市南山区南风社工服务社官网（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:51,
+      rank:51,
       title:'MAGPOC官网（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:52,
+      rank:52,
       title:'HV品牌官网（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:53,
+      rank:53,
       title:'华夏星光官网（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:54,
+      rank:54,
       title:'星翼创想官网（2019前）',
       type:'网页',
       year:2019
     },
     {
       id:55,
+      rank:55,
       title:'富涞财富官网（2019前）',
       type:'网页',
       year:2019
@@ -409,15 +471,13 @@ const List = (props) =>{
 const SelectList = [
   {
     id:0,
-    title:"默认"
+    title:"默认",
+    sort:(a,b) => a
   },
   {
     id:1,
-    title:"最新"
-  },
-  {
-    id:2,
-    title:"2019前"
+    title:"最新",
+    sort:(a,b) => a.rank - b.rank
   }
 ]
 const [selected,setSelect]  = useState(0)
@@ -466,7 +526,7 @@ function switchSelect(e) {
       <div className="caseList">
         <ul>
           {
-            caseList.map((item,index) => {
+            caseList.sort(SelectList[selected].sort).map((item,index) => {
               const _html = (
                   <li key={index} onClick={switchDetail.bind(this,item.id,item.title,item.type)}>
                     <img src={require("./assets/cover/c-"+item.id+".jpg")} alt="" />
